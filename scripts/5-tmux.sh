9a0aa6414057ab888e00da2497433db4e3eb6c98
hong-jen kao <cadeskao@gmail.com>
2024-05-01 02:07:39 +0800

    feat: introduce tmux setup script

---
+++ scripts/5-tmux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+## install Tmux Plugin Manager
+## https://github.com/tmux-plugins/tpm
+echo "[INFO][TMUX] installing tpm"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "[INFO][TMUX] tpm installed"
